a5c3c7ee779a284dcf31b525befd334e169104b0
Siyabongak <sskhumalo03@gmail.com>
2024-01-12 09:59:17 +0200

    first commit

---
+++ app/src/main/java/com/example/dieseltrak/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.dieseltrak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //Declare our varibles
+
+    EditText etOpName;
+    EditText etMachCode;
+    EditText etHours;
+    EditText etOpen;
+    EditText etClosing;
+    Button btnDispense;
+    Button btnCancel;
+    TextView tvDisplay;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //connecting our buttons to activity_main
+        etOpName = findViewById(R.id.etOpName);
+        etMachCode = findViewById(R.id.etMachCode);
+        etHours = findViewById(R.id.etHours);
+        etOpen = findViewById(R.id.etOpen);
+        etClosing = findViewById(R.id.etClosing);
+        btnDispense = findViewById(R.id.btnDispense);
+        btnCancel = findViewById(R.id.btnCancel);
+        tvDisplay =  findViewById(R.id.tvDisplay);
+
+        tvDisplay.setVisibility(View.GONE);
+
+        btnDispense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //if no values are populated
+
+                if(etOpName.getText().toString().isEmpty() || etMachCode.getText().toString().isEmpty() || etHours.getText().toString().isEmpty() || etOpen.getText().toString().isEmpty() ||
+                etClosing.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+                }else{
+                    String name = etOpName.getText().toString().trim();
+                    String code = etMachCode.getText().toString().trim();
+                    Double hours  = Double.parseDouble(etHours.getText().toString());
+                    int opening = Integer.parseInt(etOpen.getText().toString());
+                    int closing = Integer.parseInt(etClosing.getText().toString());
+
+
+                    int results = closing - opening;
+
+                    String message = "Operator Name : "+name+ "\n" + "Machine Code : " +code+ "\n" + "Total Fuel Dispensed : "+results +"\n"+ "machine hours: " +hours;
+                    tvDisplay.setText(message);
+                    tvDisplay.setVisibility(View.VISIBLE);
+
+                }
+
+
+
+
+            }
+        });
+    }
+}
